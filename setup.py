794415dc13cd2fa7ab80c01810152695d8ee89cb
Peter Darche <pdarche@gmail.com>
2024-01-03 23:52:19 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='doc_analyzer',
+    author='Peter Darche',
+    version='0.1.8',
+    packages=find_packages(),
+    install_requires=[
+        'PyMuPDF',
+        'PyPDF2',
+        'requests',
+        'langchain',
+        'llama_index==0.8.26',
+        'boto3',
+        'scikit-learn',
+        'openai==0.27.8',
+        'pydantic==1.10.12',
+        'pydub',
+        'textract',
+        'html2text',
+        'beautifulsoup4'
+    ],
+)
